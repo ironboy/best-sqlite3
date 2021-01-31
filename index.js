9d5a1260d0ca97e9d300f8a3e3171887e206e17e
ironboy <thomas@nodehill.com>
2021-01-31 19:36:27 +0100

    path fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const { Worker } = require('worker_threads');
 const inited = global.___SQL;
 module.exports = inited && require('./BestSqlite');
 if (!inited) {
   // note: require.cache used to get start script to run later
-  new Worker('./starter.js', { workerData: Object.keys(require.cache)[0] });
+  new Worker(path.join(__dirname, './starter.js'), { workerData: Object.keys(require.cache)[0] });
 }
